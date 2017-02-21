d19a52af3b74078dc57fc77bf93807cb2d981bf2
Robert Klep <robert@klep.name>
2017-02-21 20:08:03 +0100

    Remove all files related to a video

--- remove-video.py
+++ remove-video.py
@@ -2,10 +2,13 @@
 
 import xbmc, xbmcgui, xbmcvfs, glob, os.path
 
+def log(msg):
+    xbmc.log(msg = msg, level = xbmc.LOGERROR)
+
 # Get the necessary properties
-fullpath   = xbmc.getInfoLabel('ListItem.FilenameAndPath')
-dbid       = xbmc.getInfoLabel('ListItem.DBID')
-filename   = xbmc.getInfoLabel('ListItem.Filename')
+fullpath = xbmc.getInfoLabel('ListItem.FilenameAndPath')
+dbid     = xbmc.getInfoLabel('ListItem.DBID')
+filename = xbmc.getInfoLabel('ListItem.Filename')
 
 # Check if this is an actual file and the user really wants to remove it.
 if not filename or not xbmcgui.Dialog().ok('Are you sure?', 'Do you want to delete:', filename):
@@ -14,10 +17,14 @@ if not filename or not xbmcgui.Dialog().ok('Are you sure?', 'Do you want to dele
 # Delete the file from the filesystem.
 xbmcvfs.delete(fullpath)
 
-# Remove any subtitle files
+# Remove any related files (with the same prefix)
+directory = os.path.dirname(fullpath)
 root, ext = os.path.splitext(fullpath)
-for subtitle in glob.glob(root + '*.srt'):
-    xbmcvfs.delete(subtitle)
+dirs, files = xbmcvfs.listdir(directory)
+for file in files:
+    if file.startswith(os.path.basename(root)):
+        filepath = os.path.join(directory, file)
+        xbmcvfs.delete(filepath)
 
 # Also delete it from the library.
 rpc = '{ "jsonrpc": "2.0", "method": "VideoLibrary.RemoveEpisode", "params" : { "episodeid": %s }, "id" : 1 }' % dbid
